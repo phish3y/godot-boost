d924b97f817bef39f16d35a1e2b91bd7e46d8836
phish3y <logsdonevan1@gmail.com>
2026-02-10 19:37:03 -0800

    level 1/2/3

---
+++ level/landing_pad.gd
@@ -0,0 +1,4 @@
+extends CSGBox3D
+class_name LandingPad
+
+@export_file("*.tscn") var file_path
--- player.gd
+++ player.gd
@@ -1,10 +1,29 @@
-extends Node3D
+extends RigidBody3D
+
+## How much vertical force to apply when moving
+@export_range(750, 1500) var thrust: float = 1000.0
+
+## How much torque to apply when turning
+@export var torque_thrust: float = 100
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("ui_accept"):
-		position.y += delta
+	if Input.is_action_pressed("boost"): # "boost" defined in input map
+		apply_central_force(basis.y  * delta * thrust)
 	
-	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
-	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+	if Input.is_action_pressed("rotate_left"): # same
+		apply_torque(Vector3(0.0, 0.0, torque_thrust) * delta)
+	if Input.is_action_pressed("rotate_right"): # same
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust) * delta)
+
+func _on_body_entered(body: Node) -> void:
+	if "goal" in body.get_groups() && body is LandingPad:
+		complete_level(body.file_path)
+		
+	if "hazard" in body.get_groups():
+		crash_sequence()
+
+func complete_level(next_level_file: String) -> void:
+	get_tree().change_scene_to_file(next_level_file)
+	
+func crash_sequence() -> void:
+	get_tree().reload_current_scene()
